1ec25ac6a1148e8842ae275b16cdc68dc98f7759
Anthony Brian Jiménez López <60039575+BryantJim@users.noreply.github.com>
2020-10-07 18:31:35 -0400

    Creación y configuración de la Base de datos.

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,9 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using Jimenez_P1_AP2.Models;
 
 namespace Jimenez_P1_AP2.DAL
 {
     public class Contexto : DbContext
     {
+        public DbSet<Productos> Productos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite(@"Data source= Data\Parcial.db");
